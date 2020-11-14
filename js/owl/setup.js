65d63391c8df8f45019c5d85e928e66e6394f033
MathSena <MathSena07@hotmail.com>
2020-11-14 01:03:51 -0300

    Subindo projeto

---
+++ js/owl/setup.js
@@ -0,0 +1,21 @@
+
+
+autoplay:true
+autoplayTimeout:5000
+autoplayHoverPause:false
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
